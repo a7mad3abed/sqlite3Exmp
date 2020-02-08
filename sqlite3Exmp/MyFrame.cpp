5203547b7ab32932993794a5be3cbcfdfb231f47
Ahmed <31596415+a7mad3abed@users.noreply.github.com>
2020-02-08 20:42:44 +0100

    nnn

--- sqlite3Exmp/MyFrame.cpp
+++ sqlite3Exmp/MyFrame.cpp
@@ -4,5 +4,9 @@
 MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
     : wxFrame(NULL, wxID_ANY, title, pos, size)
 {
+    wxBoxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
+    wxPanel* panel01 = new wxPanel(this, wxID_ANY);
+    wxStaticText* text01 = new wxStaticText(panel01, wxID_ANY, "Text to change", );
+    wxTextCtrl* textCtl = new wxTextCtrl(panel01, wxID_ANY, "also must be changed");
 
 }
