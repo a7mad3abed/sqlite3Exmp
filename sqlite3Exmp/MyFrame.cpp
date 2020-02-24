600522178e5f379ac7a69070a5acf8027dd630ec
Ahmed Ibrahim <a7mad3abed@gmail.com>
2020-02-25 00:41:20 +0100

    adding grid in Panel

--- sqlite3Exmp/MyFrame.cpp
+++ sqlite3Exmp/MyFrame.cpp
@@ -18,20 +18,22 @@ MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
     int height = 0;
     this->DoGetSize(&width, &height);
 
+    wxPanel* panel = new wxPanel(this, wxID_ANY, wxPoint(0, 0), wxSize(300 , 100),wxALIGN_CENTER_HORIZONTAL);
     wxGridSizer* gridSizer = new wxGridSizer(2, 2, wxSize(2, 2));
     //wxBoxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
     //sizer->Add(new wxStaticText(this, wxID_ANY, "Text to change",  wxDefaultPosition, wxSize(width/2, 20)), 0, 10, wxALL);
     //sizer->Add(new wxTextCtrl(this, wxID_ANY, "also must be changed", wxDefaultPosition, wxSize(width/2,20)), 0, 10 , wxALL);
 
-    wxTextCtrl* textCtrl = new wxTextCtrl(this, wxID_ANY, "also must be changed", wxDefaultPosition, wxSize(width / 4, 20));
-    wxTextCtrl* textCtrl01 = new wxTextCtrl(this, wxID_ANY, "also must be changed", wxDefaultPosition, wxSize(width / 4, 20));
-    wxButton* button1 = new wxButton(this, BUTTON1, "add record");
-    wxButton* button2 = new wxButton(this, BUTTON2, "Show results");
+    wxTextCtrl* textCtrl = new wxTextCtrl(panel, wxID_ANY, "also must be changed", wxDefaultPosition, wxSize(width / 3, 20));
+    wxTextCtrl* textCtrl01 = new wxTextCtrl(panel, wxID_ANY, "also must be changed", wxDefaultPosition, wxSize(width / 3, 20));
+    wxButton* button1 = new wxButton(panel, BUTTON1, "add record");
+    wxButton* button2 = new wxButton(panel, BUTTON2, "Show results");
     gridSizer->Add(button1, wxALIGN_CENTER_HORIZONTAL | wxALIGN_CENTER_VERTICAL | wxALL);
-    gridSizer->Add(textCtrl, wxALIGN_CENTER_HORIZONTAL | wxALIGN_CENTER_VERTICAL | wxALL);
+    gridSizer->Add(textCtrl, wxALIGN_RIGHT | wxALIGN_CENTER_VERTICAL | wxALL);
     gridSizer->Add(button2, wxALIGN_CENTER_HORIZONTAL | wxALIGN_CENTER_VERTICAL | wxALL);
     gridSizer->Add(textCtrl01, wxALIGN_CENTER_HORIZONTAL | wxALIGN_CENTER_VERTICAL | wxALL);
-    SetSizer(gridSizer);
+    panel->SetSizerAndFit(gridSizer);
+    wxPanel* panel01 = new wxPanel(this, wxID_ANY, wxPoint(0, 0), wxSize(300 , 100));
 }
 
 void MyFrame::onButtonClicked01(wxCommandEvent& event)
